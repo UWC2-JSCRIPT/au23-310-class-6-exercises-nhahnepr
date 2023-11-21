6fff8427f6232b0448eb31fec10db312e8577fe8
nhahnepr <124095314+nhahnepr@users.noreply.github.com>
2023-11-21 10:35:47 -0800

    commit changes

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,10 +1,20 @@
+$(document).ready(function () {
 // Do not change
 document.getElementById('cat').addEventListener('click', () => {
   alert('meow!');
 });
-
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+$("a").click(function(event) {
+  event.preventDefault();
+  alert("Here\'s some info");
+});
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+$("#dog").click(function(event) {
+  event.stopPropagation();
+  alert("Bow wow!");
+});
+});
